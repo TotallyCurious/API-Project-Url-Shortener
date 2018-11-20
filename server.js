23fcd86929a3a8e7de555b399f1dfd98e6f55697
Glitch (noble-rayon) <none>
2018-11-20 08:48:22 +0000

    🐴🚐 Checkpoint ./server.js:2018030/66 ./views/index.html:2018030/161

--- server.js
+++ server.js
@@ -27,12 +27,13 @@ app.get('/', function(req, res){
 
   
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
+// app.get("/api/hello", function (req, res) {
+//   res.json({greeting: 'hello API'});
+// });
 
 app.post('/api/shorturl/new',(req, res)=>{
-  console.log(req);
+  console.log(req.body);
+  return;
 })
 
 
