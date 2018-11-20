a4f8e645a9be1c83e6e84cc54511bedd03a9f568
Glitch (noble-rayon) <none>
2018-11-20 08:37:22 +0000

    🎷🤑 Checkpoint ./server.js:2018030/89

--- server.js
+++ server.js
@@ -31,6 +31,10 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.post('/api/shorturl/new',(req, res)=>{
+  console.log(req);
+})
+
 
 app.listen(port, function () {
   console.log('Node.js listening ...');
